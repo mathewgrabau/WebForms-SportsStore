1bf4bb265e27c12718dc702c3d1d1ef79b6d3bfd
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-06-17 16:46:03 -0500

    Implemented the controls for showing the categories list.

---
+++ SportsStore/Controls/CategoryList.ascx.cs
@@ -0,0 +1,33 @@
+﻿using SportsStore.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Routing;
+
+namespace SportsStore.Controls
+{
+    public partial class CategoryList : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected IEnumerable<string> GetCategories()
+        {
+            return new Repository().Products.Select(p => p.Category).Distinct().OrderBy(x => x);
+        }
+
+        protected string CreateHomeLinkHtml()
+        {
+            string path = RouteTable.Routes.GetVirtualPath(null, null).VirtualPath;
+            return $"<a href='{path}'>Home</a>";
+        }
+
+        protected string CreateLinkHtml(string category)
+        {
+            string path = RouteTable.Routes.GetVirtualPath(null, null, new RouteValueDictionary { { "category", category }, { "page", "1" } }).VirtualPath;
+            return $"<a href='{path}'>{category}</a>";
+        }
+    }
+}
